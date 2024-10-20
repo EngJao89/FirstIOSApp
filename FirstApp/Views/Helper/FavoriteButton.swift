9204315156f3a1210fe5ad9cd82b94ccc9b35d2f
EngJao89 <eng_joao@outlook.com>
2024-10-19 23:01:51 -0300

    feat: implementa seleção de favoritos dinamicamente.

---
+++ FirstApp/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  FirstApp
+//
+//  Created by João Ricardo Martins Ribeiro on 19/10/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- FirstApp/Views/Landmarks/LandmarkDetail.swift
+++ FirstApp/Views/Landmarks/LandmarkDetail.swift
@@ -8,21 +8,32 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
 
+
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
     var body: some View {
+        @Bindable var modelData = modelData
+
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
-            
+
             CircleImage(image: landmark.image)
                 .offset(y: -130)
-                    .padding(.bottom, -130)
-            
+                .padding(.bottom, -130)
+
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -30,16 +41,14 @@ struct LandmarkDetail: View {
                 }
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
-                
+
                 Divider()
-                
+
                 Text("About \(landmark.name)")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .font(.title2)
                 Text(landmark.description)
             }
             .padding()
-            
-            Spacer()
         }
         .navigationTitle(landmark.name)
         .navigationBarTitleDisplayMode(.inline)
@@ -47,5 +56,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
